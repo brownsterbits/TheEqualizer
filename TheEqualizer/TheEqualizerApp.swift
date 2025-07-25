c7eb4d344691d17be1850a0ee93f932cd78b803f
Chad Brown <cb@xogo.io>
2025-07-24 19:46:37 -0700

    Initial Commit

---
+++ TheEqualizer/TheEqualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheEqualizerApp.swift
+//  TheEqualizer
+//
+//  Created by Chad Brown on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TheEqualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheEqualizerTests/TheEqualizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheEqualizerTests.swift
+//  TheEqualizerTests
+//
+//  Created by Chad Brown on 7/24/25.
+//
+
+import Testing
+@testable import TheEqualizer
+
+struct TheEqualizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
